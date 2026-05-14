0e5fbf9af554752afe5ea0213ef116e98f781d8f
Nandini <144013647+nandini-nan@users.noreply.github.com>
2026-05-14 00:34:03 -0500

    Add files via upload

---
+++ file handling.py
@@ -0,0 +1,49 @@
+#file handling
+#write()
+'''a=open("nandu.txt","w")
+a.write("codegnan")
+a.close()'''
+
+
+'''a=open("nandu.txt","w")
+a.write("python")
+a.close()'''
+
+#append()
+'''a=open("nandu.txt","a")
+a.write("/tcodegnan it solutions")
+a.close()'''
+
+
+'''a=open("nandu.txt","a")
+a.write(input("enter the data"))
+a.close()'''
+
+'''a=open("nandu.txt","w")
+b=input("Data")
+a.write(b)
+a.close()'''
+
+#read()
+'''a=open("nandu.txt")
+#print(a.read()) #it will display entire content
+#print(a.readline()) # it will display first line
+#print(a.readlines()) #it will display with \n
+print(a.read(15)) #it will display no.of characters'''
+
+#writelines()->it makes every object side by side
+'''names=["nandu","nandini","deepu","mouni","ramya"]
+a=open("nandu.txt","w")
+a.writelines(names)
+a.close()'''
+
+'''names=["nandu","nandini","deepu","mouni","ramya"]
+a=open("nandu.txt","w")
+a.write("\n".join(names)) 
+a.close()'''
+
+'''a=open("data.py")
+print(a.read())'''
+
+a=open(C:\Users\DELL\Desktop\PYTHON.py
+print(a.read())
