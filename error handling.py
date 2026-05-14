c2727e0743b665207b8b4493784d615ad41cc4d8
Nandini <144013647+nandini-nan@users.noreply.github.com>
2026-05-14 00:33:31 -0500

    Add files via upload

---
+++ error handling.py
@@ -0,0 +1,25 @@
+#error handling
+#syntax error
+'''a=int(input())
+b=int(input())
+print(a//b)'''
+
+#run_time error
+'''a=int(input("a value"))
+b=int(input("b value"))
+print(a//b)'''
+
+#logical error
+'''a=10
+b=20
+print(a-b)'''
+
+'''a=3
+b=6
+if a<b:
+    print("true")'''
+
+'''a=3
+b=6
+if a>b:
+    print("true")'''
